36ade21e6d9912816f2dc63c0d6c0986263288de
Krzych <krzych.pierzchala@gmail.com>
2017-10-25 07:25:09 +0200

    Set a project in js file

---
+++ program.js
@@ -0,0 +1,29 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+	var input = process.stdin.read();
+	if(input !== null) { 
+		var instruction = input.toString().trim();
+/*		if(instruction === '/exit') {
+			process.stdout.write('Quitting app!\n');
+			process.exit();
+		}
+		else {
+			process.stderr.write('Wrong instruction!\n');
+		}
+*/		switch(instruction) {
+			case '/exit':
+				process.stdout.write('Quitting app!\n');
+				process.exit();
+				break;
+			case '/version':
+				console.log(process.versions);
+				break;
+			case '/lang':
+				process.stdout.write(process.env.LANG + '\n');
+				break;
+			default:
+				process.stderr.write('Wrong instruction!\n');
+		}
+	}
+});
