ab67f70a21e4a7f4543228fc72246adf1a2eed0a
Krzych <krzych.pierzchala@gmail.com>
2017-10-30 14:58:48 +0100

    Added function getOsInfo from os module

--- program.js
+++ program.js
@@ -1,3 +1,7 @@
+var os = require('os');
+
+console.log(os.type());
+
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
@@ -15,8 +19,29 @@ process.stdin.on('readable', function() {
       case '/lang':
         process.stdout.write(process.env.LANG + '\n');
         break;
+      case '/getOsInfo':
+        getOsInfo();
+        break;
       default:
         process.stderr.write('Wrong instruction!\n');
     }
   }
+
+  function getOsInfo() {
+    var type = os.type();
+    var release = os.release();
+    var cpu = os.cpus()[0];
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+    if (type == 'Darwin') type = 'OSX';
+    else if (type == 'Windows_NT') type = 'Windows';
+    console.log('System: ', type);
+    console.log('Release: ', release);
+    console.log('CPU model: ', cpu.model);
+    console.log('CPU speed: ', cpu.speed);
+    console.log('Uptime: ~', (uptime/60).toFixed(0), ' min / ', (uptime/3600).toFixed(0), ' hours / ', (uptime/86400).toFixed(0), ' days');
+    console.log('User name: ', userInfo.username);
+    console.log('Home dir: ', userInfo.homedir);
+  }
+
 });
